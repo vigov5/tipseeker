0684e14622c745371094d8d04bc97a081eb5c563
Nguyen Anh Tien <nguyenanhtien2210@gmail.com>
2020-10-16 00:15:30 +0700

    Add copy URL button

--- frontend/src/Tip.svelte
+++ frontend/src/Tip.svelte
@@ -1,10 +1,13 @@
 <script>
   import { fade, fly } from "svelte/transition";
+  import CopyToClipboard from "svelte-copy-to-clipboard";
 
   export let tip;
   export let show_all;
   export let setMedia;
   export let markRead;
+
+  let copyText = "Copy URL";
 
   const CATEGORYS = {
     0: "web",
@@ -74,61 +77,84 @@
         <p class="mb-3 leading-relaxed whitespace-pre-wrap">
           {@html tip.content}
         </p>
-        <div class="flex flex-wrap items-center">
+        <div class="flex flex-wrap items-center justify-between">
           <span class="px-1 text-white bg-indigo-500 rounded-lg">
             {CATEGORYS[tip.category]}
           </span>
-          {#if !tip.read}
+          <div>
             <span
-              class="inline-flex items-center py-1 pr-3 ml-auto mr-3 text-sm leading-none text-red-600 border-r-2 border-gray-300 lg:ml-auto md:ml-0">
-              <svg
-                class="w-4 h-4 mr-1"
-                stroke="currentColor"
-                stroke-width="2"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                viewBox="0 0 24 24">
-                <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
-                <circle cx="12" cy="12" r="3" />
-              </svg>
-              Unread
+              class="py-1 pr-3 mr-3 text-sm leading-none text-gray-600 border-r-2 border-gray-300 lg:ml-auto md:ml-0">
+              <CopyToClipboard
+                text={tip.url}
+                on:copy={() => (copyText = 'Copied!')}
+                let:copy>
+                <button on:click={copy} class="inline-flex items-center">
+                  <svg
+                    class="w-4 h-4 mr-1"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"><path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3" /></svg>
+                  {copyText}
+                </button>
+              </CopyToClipboard>
             </span>
-          {:else}
-            <span
-              class="inline-flex items-center py-1 pr-3 ml-auto mr-3 text-sm leading-none text-green-500 border-r-2 border-gray-300 lg:ml-auto md:ml-0">
-              <svg
-                class="w-4 h-4 mr-1"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg">
-                <path
+            {#if !tip.read}
+              <span
+                class="inline-flex items-center py-1 pr-3 ml-auto mr-3 text-sm leading-none text-red-600 border-r-2 border-gray-300 lg:ml-auto md:ml-0">
+                <svg
+                  class="w-4 h-4 mr-1"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  fill="none"
                   stroke-linecap="round"
                   stroke-linejoin="round"
+                  viewBox="0 0 24 24">
+                  <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
+                  <circle cx="12" cy="12" r="3" />
+                </svg>
+                Unread
+              </span>
+            {:else}
+              <span
+                class="inline-flex items-center py-1 pr-3 ml-auto mr-3 text-sm leading-none text-green-500 border-r-2 border-gray-300 lg:ml-auto md:ml-0">
+                <svg
+                  class="w-4 h-4 mr-1"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+                Read
+              </span>
+            {/if}
+            <span
+              class="inline-flex items-center text-sm leading-none text-gray-600">
+              <a href={tip.origin} target="_blank" class="flex items-center">
+                <svg
+                  class="w-4 h-4 mr-1"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
                   stroke-width="2"
-                  d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-              </svg>
-              Read
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round">
+                  <path d="M5 12h14" />
+                  <path d="M12 5l7 7-7 7" />
+                </svg>
+                Source
+              </a>
             </span>
-          {/if}
-          <a
-            href={tip.origin}
-            target="_blank"
-            class="inline-flex items-center text-sm leading-none text-gray-600">
-            Source
-            <svg
-              class="w-4 h-4 ml-2"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              stroke-width="2"
-              fill="none"
-              stroke-linecap="round"
-              stroke-linejoin="round">
-              <path d="M5 12h14" />
-              <path d="M12 5l7 7-7 7" />
-            </svg>
-          </a>
+          </div>
         </div>
       </div>
     </div>
